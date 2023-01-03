16dcb2011b6821a793ed57c53fb876cc3503f329
devkitae <devkitae@gmail.com>
2023-01-03 09:55:16 +0900

    Config #1 - spring option
    스프링 기초설정

---
+++ src/main/java/com/kt/springex1/config/Config.java
@@ -0,0 +1,12 @@
+package com.kt.springex1.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created By Cheetah on 2023-01-03.
+ */
+@ComponentScan("com.kt.springex1")
+@Configuration
+public class Config {
+}
